7c45d690439db8f64e8c0edd21effcf2465f7743
Cina Saffary <csaffary@umbel.com>
2015-03-01 23:58:36 -0600

    fixing bug in prefixer

--- src/js/util/css-prefixer.js
+++ src/js/util/css-prefixer.js
@@ -1,10 +1,10 @@
 function prefix(property) {
-  var capitalizedProperty = property[0].toUpperCase + property.slice(1)
+  var capitalizedProperty = property[0].toUpperCase() + property.slice(1)
   var styles = Object.keys(document.body.style)
 
   return ~styles.indexOf(property) && property
-      || ~styles.indexOf('Webkit' + capitalizedProperty) && 'Webkit' + capitalizedProperty
-      || ~styles.indexOf('Moz'    + capitalizedProperty) && 'Moz   ' + capitalizedProperty
+      || ~styles.indexOf('webkit' + capitalizedProperty) && 'webkit' + capitalizedProperty
+      || ~styles.indexOf('moz'    + capitalizedProperty) && 'moz   ' + capitalizedProperty
       || ~styles.indexOf('ms'     + capitalizedProperty) && 'ms'     + capitalizedProperty
       || ~styles.indexOf('O'      + capitalizedProperty) && 'O'      + capitalizedProperty
 }
