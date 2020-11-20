cc3d703b720cf5ad3def5f60558125f37338fa3f
Cina Saffary <itscina@gmail.com>
2020-11-20 13:27:21 -0600

    Use document.documentElement instead of document.body for scroll properties

--- src/js/page.js
+++ src/js/page.js
@@ -9,12 +9,12 @@ var Page = function (WIDTH, HEIGHT) {
 
 Page.prototype.init = function () {
   this.width = this.WIDTH * window.innerHeight / this.HEIGHT
-  this.scrollHeight = document.body.scrollHeight - window.innerHeight
+  this.scrollHeight = document.documentElement.scrollHeight - window.innerHeight
   this.scrollWidth = this.width - window.innerWidth
 }
 
 Page.prototype.updatePosition = function () {
-  this.position = bound(document.body.scrollTop / this.scrollHeight, 0, 100) * 100
+  this.position = bound(document.documentElement.scrollTop / this.scrollHeight, 0, 100) * 100
 }
 
 module.exports = Page
